ea7bcd1a7f82f7b405aac6a24d389c77d824e4e4
Matt Stone <mattstone22133@gmail.com>
2019-07-11 19:40:01 -0400

    updating bitmap font letters.

--- WebGL 1.0 experiments/BitmapFontTesting/index.js
+++ WebGL 1.0 experiments/BitmapFontTesting/index.js
@@ -149,7 +149,7 @@ class Game
         
         this.bitmapFont = new Montserrat_BMF(this.gl, "../shared_resources/Textures/Fonts/Montserrat_ss_alpha_1024x1024_wb.png");
         // this.bitmapFont = new Montserrat_BMF(this.gl, "../shared_resources/Grass2.png");
-        this.testGlyphRenderer = this.bitmapFont.getGlyphFor("j");
+        this.testGlyphRenderer = this.bitmapFont.getGlyphFor("r");
         
         this.testGlyphs = ["a","b","c","d","e","f","h","g","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z","A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","0","1","2","3","4","5","6","7","8","9","-","=","!","@","#","$","%","^","&","*","(",")","_","+",";",":","'","\"","[","{","]","}","/","?",".",">",",","<","\\","|","`","~","÷","©","®","ç","â","à","é","è","ê","ë","î","ï","ô","û","ù","ü",];
 
